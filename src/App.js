c9072776d482faee1f870de2d4397bc8fbc31f40
trangphamptit <thuytrang.bcvt2015@gmail.com>
2019-06-17 17:45:54 +0700

    init_project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./Pages/HomePage/HomePage";
+import PhonePage from "./Pages/PhonePage/PhonePage";
+import CodePage from "./Pages/CodePage/CodePage";
+import UserPage from "./Pages/UserPage/UserPage";
+import ShowLocal from "./components/ShowLocal/ShowLocal";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MainPage from "./Pages/MainPage/MainPage";
+import "./Pages/MainPage/MainPage.scss";
+import ShowMapPage from "./Pages/ShowMapPage/ShowMapPage";
+import ShowDestination from "./Pages/ShowDestination/ShowDestination";
+
+// import ShowMapPage from "./Pages/ShowMapPage/ShowMapPage";
+function App() {
+  return (
+    <BrowserRouter>
+      <MainPage>
+        <Switch>
+          {/* <Route path="/showmappage" component={ShowMapPage} /> */}
+          <Route path="/showlocal" component={ShowLocal} />
+          <Route path="/phonepage" component={PhonePage} />
+          <Route path="/codepage" component={CodePage} />
+          <Route path="/userpage" component={UserPage} />
+          <Route path="/" exact component={CodePage} />
+          <Route path="/showdestination" component={ShowDestination} />
+          <Route path="/showmappage" component={ShowMapPage} />
+          {/* <Route path="/phonepage" component={PhonePage} />
+          <Route path="/codepage" component={CodePage} />
+          <Route path="/inforpage" component={InforPage} /> */}
+        </Switch>
+      </MainPage>
+    </BrowserRouter>
+  );
+}
+
+export default App;
